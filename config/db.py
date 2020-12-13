232657b656ac7dbb4f2c9917d76ce5157770dad3
cdevalero <onvalero@gmail.com>
2020-12-13 12:38:25 -0400

    first commit

---
+++ config/db.py
@@ -0,0 +1,10 @@
+POSTGRES = {
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'proyecto',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
